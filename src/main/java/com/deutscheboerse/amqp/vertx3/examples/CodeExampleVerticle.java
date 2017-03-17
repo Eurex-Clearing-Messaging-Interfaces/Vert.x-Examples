fd6bba89e04d72f55bbff92a219694ee13728617
Jakub Scholz <www@scholzj.com>
2017-03-17 22:42:02 +0100

    Update jsse branch to Vert.x 3.4.1

--- src/main/java/com/deutscheboerse/amqp/vertx3/examples/CodeExampleVerticle.java
+++ src/main/java/com/deutscheboerse/amqp/vertx3/examples/CodeExampleVerticle.java
@@ -146,7 +146,7 @@ public class CodeExampleVerticle extends AbstractVerticle {
         AliasProvider.setAsDefault();
 
         LOG.info("Opening connection to " + config().getString("amqp.hostname", "localhost") + ":" + config().getInteger("amqp.port", 5671) + " with server certificate " + config().getString("amqp.serverCert") + ", client certificate " + config().getString("amqp.clientCert") + " and client key " +  config().getString("amqp.clientKey"));
-        ProtonClientOptions options = new ProtonClientOptions().setAllowedSaslMechanisms("EXTERNAL").setIdleTimeout(0).setSsl(true).setPemKeyCertOptions(new PemKeyCertOptions().setCertPath(config().getString("amqp.clientCert")).setKeyPath(config().getString("amqp.clientKey"))).setPemTrustOptions(new PemTrustOptions().addCertPath(config().getString("amqp.serverCert")));
+        ProtonClientOptions options = new ProtonClientOptions().addEnabledSaslMechanism("EXTERNAL").setIdleTimeout(0).setSsl(true).setPemKeyCertOptions(new PemKeyCertOptions().setCertPath(config().getString("amqp.clientCert")).setKeyPath(config().getString("amqp.clientKey"))).setPemTrustOptions(new PemTrustOptions().addCertPath(config().getString("amqp.serverCert")));
 
         proton.connect(options, config().getString("amqp.hostname", "localhost"), config().getInteger("amqp.port", 5671), connectResult -> {
             if (connectResult.succeeded()) {
